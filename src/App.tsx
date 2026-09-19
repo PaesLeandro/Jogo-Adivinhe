b3daf5a4e41af1bc8d989261d65a6dbbdc41d72e
Leandro <leandromkt.tr@gmail.com>
2026-09-19 19:32:44 -0300

    Firt commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import styles from "./app.module.css";
+
+import { Input } from "./components/Input";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Header } from "./components/Header";
+
+export default function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
+
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={(handleRestartGame)} />
+        
+        <Tip tip="Uma das linguagem de programação mais utilizadas" />
+
+      <div className={styles.word}>
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
+      </div> 
+
+      <h4>Palpite</h4>
+
+      <div>
+        <Input autoFocus maxLength={1} placeholder="?"/>
+      </div>
+      </main>
+    </div>
+  )
+}
