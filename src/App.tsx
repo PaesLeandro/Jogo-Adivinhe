0dee86b8fa4e2326aa7f3374d5898d9e4c9d8120
Leandro <leandromkt.tr@gmail.com>
2026-09-20 01:17:13 -0300

    Criando as funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,139 @@
 import styles from "./app.module.css";
+import { useEffect, useState } from "react";
+
+import { WORDS, type Challenge } from "./utils/words";
 
 import { Tip } from "./components/Tip";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import { Letter } from "./components/Letter";
 import { Header } from "./components/Header";
-import { LettersUsed } from "./components/LettersUsed";
+import { LettersUsed, type LetterUserProps } from "./components/LettersUsed";
 
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [lettersUsed, setLetterUsed] = useState<LetterUserProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
+  const ATTEMPTS_MARGIN =  5
+
   function handleRestartGame() {
-    alert("Reiniciar o jogo");
+    const isConfirmed = window.confirm("Você tem certeza que deseja reiniciar o jogo?");
+    
+    if (isConfirmed) {
+      startGame();
+    }
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setScore(0);
+    setLetter('');
+    setLetterUsed([]);
+  };
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra");
+    }
+
+    const value = letter.trim().toUpperCase();
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value);
+
+    if (exists) {
+      setLetter("");
+      return alert("Você já utilizou a letra " + value);
+    }
+
+    const hits = challenge.word
+    .toUpperCase()
+    .split("")
+    .filter((char) => char === value).length;
+
+    const correct = hits > 0;
+    const currentScore = score + hits;
+
+    setLetterUsed((prevState) => [...prevState, { value, correct }]);
+    setScore(currentScore);
+
+    setLetter("");
+  }
+
+function endGame(message: string) {
+  alert(message);
+  startGame();
+}
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  useEffect(() => {
+    if (!challenge) {
+      return;
+    }
+    setTimeout(() => {
+      if(score === challenge.word.length) {
+        endGame("Parabéns! Você descobriu a palavra!");
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPTS_MARGIN;
+      if(letterUsed.length === attemptLimit) {
+        return endGame("Que pena, você usou todas as tentativas!");
+      }
+
+    }, 200);
+  }, [score, lettersUsed.length]);
+
+  if (!challenge) {
+    return null;
+  }
 
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={(handleRestartGame)} />
+        <Header 
+        current={lettersUsed.length} 
+        max={challenge.word.length + ATTEMPTS_MARGIN} 
+        onRestart={(handleRestartGame)} />
         
-        <Tip tip="Uma das linguagem de programação mais utilizadas" />
+        <Tip tip={challenge.tip} />
 
       <div className={styles.word}>
-        <Letter value="R" />
-        <Letter value="E" />
-        <Letter value="A" />
-        <Letter value="C" />
-        <Letter value="T" />
+        {challenge.word.split("").map((letter, index) => {
+          const letterUsed = lettersUsed.find(
+            (used) => used.value.toUpperCase() === letter.toUpperCase());
+
+          return <Letter key={index} value={letterUsed?.value} color={letterUsed?.correct ? "correct" : "default"}
+          />;
+      })}
       </div> 
 
       <h4>Palpite</h4>
 
       <div className={styles.guess}>
-        <Input autoFocus maxLength={1} placeholder="?"/>
-        <Button title="Confirmar" />
+        <Input 
+        autoFocus 
+        maxLength={1} 
+        placeholder="?" 
+        value={letter}
+        onChange={(e) => setLetter(e.target.value)}
+        />
+
+        <Button title="Confirmar" onClick={handleConfirm} />
       </div>
 
-      <LettersUsed />
+      <LettersUsed data={lettersUsed}/>
       </main>
     </div>
   )
